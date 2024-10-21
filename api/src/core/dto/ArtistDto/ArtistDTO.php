15c8b56b7436d49158f9541e2316398deb7516d2
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-21 14:52:23 +0200

    update artistdto for images

--- api/src/core/dto/ArtistDto/ArtistDTO.php
+++ api/src/core/dto/ArtistDto/ArtistDTO.php
@@ -9,12 +9,14 @@ class ArtistDTO extends DTO
     protected string $id;
     protected string $name;
     protected string $style;
+    protected string $image;
 
     public function __construct(Artist $a)
     {
         $this->id = $a->getId();
         $this->name = $a->getName();
         $this->style = $a->getStyle(); 
+        $this->image = $a->getImage();
 }
 
     public function getId(): string
@@ -30,5 +32,10 @@ class ArtistDTO extends DTO
     public function getStyle(): string
     {
         return $this->style;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
     }
 }
