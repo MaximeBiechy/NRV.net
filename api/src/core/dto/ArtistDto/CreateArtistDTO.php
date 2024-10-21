fbde66b70a47ffe30a7bc3daab205cf44922c0c4
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-21 13:46:14 +0200

    dto

--- api/src/core/dto/ArtistDto/CreateArtistDTO.php
+++ api/src/core/dto/ArtistDto/CreateArtistDTO.php
@@ -6,21 +6,14 @@ use nrv\core\dto\DTO;
 
 class CreateArtistDTO extends DTO
 {
-    protected string $id;
     protected string $name;
     protected string $style;
 
     public function __construct(Artist $a)
     {
-        $this->id = $a->getId();
         $this->name = $a->getName();
         $this->style = $a->getStyle(); 
 }
-
-    public function getId(): string
-    {
-        return $this->id;
-    }
 
     public function getName(): string
     {
