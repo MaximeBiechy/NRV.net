505b8bf11ea545143df7ba9c293faf4ef874a130
VadimElFamoso <vadimprofessionel@gmail.com>
2024-10-21 17:01:40 +0200

    fetches base

---
+++ frontend/js/fetches.js
@@ -0,0 +1,42 @@
+console.log('fetches.js loaded');
+
+//Récupérer l'ensemble des spectacles :
+async function fetchShows() {
+  try {
+    const response = await fetch('http://localhost:21000/shows');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+//Récupérer les détails d'une soirée par son id :
+async function fetchPartyById(id) {
+  try {
+    const response = await fetch(`http://localhost:21000/parties/${id}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+//Récupérer l'ensemble des spectacles et les détails d'une soirée par l'id du spectacle :
+async function fetchShowPartyById(showId) {
+  try {
+    const response = await fetch(`http://localhost:21000/shows/${showId}/party`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
