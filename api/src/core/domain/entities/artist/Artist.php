3503dffff1a4372317f83e99ebf56995adf57b5c
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-10-21 14:16:33 +0200

    Artist: fix

--- api/src/core/domain/entities/artist/Artist.php
+++ api/src/core/domain/entities/artist/Artist.php
@@ -7,20 +7,13 @@ use nrv\core\domain\entities\Entity;
 
 class Artist extends Entity
 {
-    private string $id;
     private string $name;
     private array $styles;
 
-    public function __construct(string $id, string $name, array $styles)
+    public function __construct(string $name, array $styles)
     {   
-        $this->id = $id;
         $this->name = $name;
         $this->styles = $styles;
-    }
-
-    public function getId(): string
-    {
-        return $this->id;
     }
 
     public function getName(): string
