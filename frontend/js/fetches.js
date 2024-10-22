adda010645e1c8b73eb9b208e8466f7eb346f60a
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-22 16:40:24 +0200

    fetches ex 3

--- frontend/js/fetches.js
+++ frontend/js/fetches.js
@@ -1,5 +1,6 @@
 //loader
   var loader = document.querySelector('.loader');
+
 // Récupérer l'ensemble des spectacles :
 async function fetchShows() {
   try {
@@ -8,7 +9,6 @@ async function fetchShows() {
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
-
       var template = Handlebars.compile(document.querySelector('.shows').innerHTML);
       var filledTemplate = template(data);
       document.querySelector('.shows').innerHTML = filledTemplate;
@@ -72,19 +72,57 @@ async function fetchShowPartyById(showId) {
 }
 
 //Vadim : Handlebars pour les soirées (question 2) :
-// function fillPartyTemplate(data) {
-//   var template = Handlebars.compile(document.querySelector('.party').innerHTML);
-//   var filledTemplate = template(data);
-//   document.querySelector('.party').innerHTML = filledTemplate;
-// }
+function fillPartyTemplate(data) {
+  var template = Handlebars.compile(document.querySelector('.party').innerHTML);
+  var filledTemplate = template(data);
+  document.querySelector('.party').innerHTML = filledTemplate;
+}
 
-// (async () => {
-//   const data = await fetchPartyById('a0b7566b-6fdd-4e34-bbab-41d882de9c07');
-//   let new_date = new Date(data.party.date.date).toLocaleDateString('fr-FR', { weekday: 'short', day: '2-digit', month: '2-digit' });
-//   data.party.date.date = new_date;
-//   fillPartyTemplate(data);
-// })();
+(async () => {
+  const data = await fetchPartyById('a0b7566b-6fdd-4e34-bbab-41d882de9c07');
+  let new_date = new Date(data.party.date.date).toLocaleDateString('fr-FR', { weekday: 'short', day: '2-digit', month: '2-digit' });
+  data.party.date.date = new_date;
+  fillPartyTemplate(data);
+})();
+// Fonction pour ajouter des gestionnaires d'événements aux cartes de spectacle
 
 
 fetchShows();
 fetchArtists();
+
+async function fetchShowInfo(id) {
+  console.log('Fetching show info for show with id:', id);
+  try {
+    const response = await fetch(`http://localhost:21000/shows/${id}/party/`, { headers: { 'Origin': 'http://localhost' }});
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+
+    var templateSource = document.querySelector('.showInfo').innerHTML;
+    var template = Handlebars.compile(templateSource);
+    var filledTemplate = template(data);
+    document.querySelector('.showInfo').innerHTML = filledTemplate;
+  }
+  catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+
+
+window.onload = function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const showId = urlParams.get('id');
+  if (showId) {
+    console.log('Show id found in URL:', showId);
+    fetchShowInfo(showId);
+  } else {
+    console.log('No show id found in URL');
+  }
+}
+
+
+
+
