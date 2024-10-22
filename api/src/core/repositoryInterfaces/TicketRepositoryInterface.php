7d222f9110cb37b0be91e2996de593c5214a8932
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-10-22 15:55:05 +0200

    Création des méthodes du pdo

--- api/src/core/repositoryInterfaces/TicketRepositoryInterface.php
+++ api/src/core/repositoryInterfaces/TicketRepositoryInterface.php
@@ -7,7 +7,11 @@ use nrv\core\domain\entities\ticket\Ticket;
 
 interface TicketRepositoryInterface
 {
-    public function save(Ticket $ticket): string;
+    public function saveTicket(Ticket $ticket): string;
+    public function saveSoldTicket(SoldTicket $soldTicket): string;
     public function getTicketById(string $id): Ticket;
-    public function getTicketsByUserID(string $userID): array;
+    public function getSoldTicketById(string $id): SoldTicket;
+    public function getSoldTicketsByUserID(string $userID): array;
+    public function getTicketsByCardID(string $cardID): array;
+    public function addTicketToCard(string $ticketID, string $cardID): void;
 }
