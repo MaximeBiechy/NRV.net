f7929dd2126a55844fe769010b589fe8b48e060a
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2024-10-24 14:42:57 +0200

    récupe des données pour le benj et son profil

---
+++ frontend/js/profil.js
@@ -0,0 +1,34 @@
+'use strict';
+
+async function fetchTickets(id_user) {
+    const response_tickets = await fetch(`http://localhost:21000/users/${id_user}/sold_tickets`);
+    if (!response_tickets.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response_tickets.json();
+    let result = data;
+    for (let i = 0; i < data.sold_tickets.length; i++) {
+        const response_party = await fetch(`http://localhost:21000${(data.sold_tickets[i].links.party.href)}`);
+        if (!response_party.ok) {
+            throw new Error('Network response was not ok');
+        }
+        result.sold_tickets[i].party = await response_party.json();
+    }
+
+    if(data.length === 0) {
+        document.querySelector('#templateTicket').innerHTML = 'Vous avez aucun billets';
+        loader.style.display = 'none';
+        return;
+    }
+    else {
+        var templateSource = document.querySelector('#templateTicket').innerHTML;
+        var template = Handlebars.compile(templateSource);
+        var filledTemplate = template(result);
+        document.querySelector('#templateTicket').innerHTML = filledTemplate;
+        loader.style.display = 'none';
+    }
+}
+
+if (localStorage.getItem('id_user')) {
+    fetchTickets(localStorage.getItem('id_user'));
+}
