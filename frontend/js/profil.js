cb417dad9614c6e83dd311cd2ed1ea742937acea
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-24 15:02:18 +0200

    js ticket

--- frontend/js/profil.js
+++ frontend/js/profil.js
@@ -1,33 +1,33 @@
 'use strict';
 
-if (!localStorage.getItem('id_user')) {
-    window.route('/login');
-}else{
-    fetchUser(localStorage.getItem('id_user'));
-}
-
-
 async function fetchTickets(id_user) {
-    const response_tickets = await fetch(`http://localhost:21000/users/${id_user}/sold_tickets`);
+    const response_tickets = await fetch(`http://localhost:21000/users/${id_user}/sold_tickets`, { 
+        headers: { 'Origin': 'http://localhost:21001' },
+        mode: 'no-cors' 
+    });
+    console.log(response_tickets);
     if (!response_tickets.ok) {
         throw new Error('Network response was not ok');
     }
     const data = await response_tickets.json();
     let result = data;
+    console.log(data);
     for (let i = 0; i < data.sold_tickets.length; i++) {
-        const response_party = await fetch(`http://localhost:21000${(data.sold_tickets[i].links.party.href)}`);
+        const response_party = await fetch(`http://localhost:21000${(data.sold_tickets[i].links.party.href)}`, { 
+            headers: { 'Origin': 'http://localhost:21001' },
+            mode: 'no-cors' 
+        });
         if (!response_party.ok) {
             throw new Error('Network response was not ok');
         }
         result.sold_tickets[i].party = await response_party.json();
     }
 
-    if(data.length === 0) {
+    if (data.length === 0) {
         document.querySelector('#templateTicket').innerHTML = 'Vous avez aucun billets';
         loader.style.display = 'none';
         return;
-    }
-    else {
+    } else {
         var templateSource = document.querySelector('#templateTicket').innerHTML;
         var template = Handlebars.compile(templateSource);
         var filledTemplate = template(result);
@@ -36,3 +36,10 @@ async function fetchTickets(id_user) {
     }
 }
 
+
+if (!localStorage.getItem('id_user')) {
+    window.route('/login');
+}else{
+    console.log(localStorage.getItem('id_user'));
+    fetchTickets(localStorage.getItem('id_user'));
+}
